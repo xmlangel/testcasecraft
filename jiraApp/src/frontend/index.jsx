f6273463bf5cf3afa0c5a9c15bce494f84c30c0f
kwangmyungKim <kwangmyung.kim@gmail.com>
2026-03-04 19:17:29 +0900

    feat: 새로운 Jira 애플리케이션을 추가하고 관련 문서를 생성합니다.

--- jiraApp/src/frontend/index.jsx
+++ jiraApp/src/frontend/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ForgeReconciler, { Text, Heading, Stack } from '@forge/react';
+import ForgeReconciler, { Text, Heading, Stack, Link } from '@forge/react';
 
 const App = () => {
   return (
@@ -7,6 +7,9 @@ const App = () => {
       <Heading size="medium">testcasecraft Jira 연동 확인</Heading>
       <Text>UI Kit 2로 업그레이드하여 렌더링 중입니다.</Text>
       <Text>testcasecraft와 연동 준비가 되었습니다.</Text>
+      <Link href="http://tc.skaiworldwide.co.kr:8080/projects/9c1ae80f-b3ca-4546-afca-b8d895ae2908/executions/627d28e6-31cb-4be5-9496-7aa4575b1071">
+        testcasecraft 실행 결과 확인
+      </Link>
     </Stack>
   );
 };
