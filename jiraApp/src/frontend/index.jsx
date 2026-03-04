9391bcd22fdb53cba80968a812457f3d886e222d
kwangmyungKim <kwangmyung.kim@gmail.com>
2026-03-04 20:38:24 +0900

    feat: Jira 앱 제목을 업데이트하고, 외부 fetch 클라이언트를 추가하며, testcasecraft 프론트엔드 링크와 텍스트를 수정합니다.

--- jiraApp/src/frontend/index.jsx
+++ jiraApp/src/frontend/index.jsx
@@ -4,11 +4,12 @@ import ForgeReconciler, { Text, Heading, Stack, Link } from '@forge/react';
 const App = () => {
   return (
     <Stack>
-      <Heading size="medium">testcasecraft Jira 연동 확인</Heading>
-      <Text>UI Kit 2로 업그레이드하여 렌더링 중입니다.</Text>
-      <Text>testcasecraft와 연동 준비가 되었습니다.</Text>
-      <Link href="http://tc.skaiworldwide.co.kr:8080/projects/9c1ae80f-b3ca-4546-afca-b8d895ae2908/executions/627d28e6-31cb-4be5-9496-7aa4575b1071">
-        testcasecraft 실행 결과 확인
+      <Text>아이디 패스워드는 admin/admin123 입니다.</Text>
+      <Link 
+        href="http://tc.skaiworldwide.co.kr:8080"
+        openNewTab={true}
+      >
+        testcasecraft 접속
       </Link>
     </Stack>
   );
